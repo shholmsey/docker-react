b6a5452aeb01354f5ac600213b616fa72dbfd5c9
shholmsey <shholmsey@gmail.com>
2020-11-28 23:06:28 +0200

    asdasd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         moby
+         moby latest
         </p>
         <a
           className="App-link"
