18e4066fd738484586991fbc6902439b6f996983
shholmsey <shholmsey@gmail.com>
2020-11-29 00:13:19 +0200

    azc

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         moby latest
+         moby latest change
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders without crashing', () => {
+test('sdf sd', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
