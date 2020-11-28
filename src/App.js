c590f0004dc729a51b3d5e1e39734f24bc795146
shholmsey <shholmsey@gmail.com>
2020-11-28 21:41:23 +0200

    asdas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         cosmin
+         moby
         </p>
         <a
           className="App-link"
